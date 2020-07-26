019376246bb6c376518bfadb9ec792dbbc7738f9
wallwpcab <walihullah@gmail.com>
2020-07-26 16:58:23 +0600

    Add files via upload

---
+++ zulkarnine_prac_fifthlec_CarInterface.java
@@ -0,0 +1,8 @@
+package FifthLecture;
+public interface CarInterface {
+	public void driveCar(int drivindTimeInSecond);
+	public void addAmountOfFuel();
+	public int getManufacturingYear();
+	public String gerCarModelName();
+	  
+}
